f48755793a330cd29ebc3d610db491fcc003c0e4
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-06-12 13:13:19 -0500

    Updated main handelbars.

--- models/user.js
+++ models/user.js
@@ -19,21 +19,24 @@ User.init(
         username: {
             type: DataTypes.STRING,
             allowNull: false,
-            unique: true,
         },
         password: {
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
-            len: [8],
+                len: [8],
+            },
         },
     },
-  },
-  {
+    {
     hooks: {
         async beforeCreate(newUserData) {
             newUserData.password = await bcrypt.hash(newUserData.password, 10);
             return newUserData;
+        },
+        async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
         },
     },
         sequelize,
