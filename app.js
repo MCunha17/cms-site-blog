ec85e140576f9a6eb996634d348ab1725b37d9ab
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-06-12 12:29:47 -0500

    Added index controller.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const exphbs = require('express-handlebars');
 const path = require('path');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const { sequelize } = require('./connection');
+const { sequelize } = require('./config/connection');
 
 const app = express();
 
