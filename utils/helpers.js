246d9b742dc586b3f9421476fc09ece3cdb02ce7
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-06-12 13:23:14 -0500

    Added helpers.

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,49 +1,20 @@
-const bcrypt = require('bcrypt');
-const User = require('../models/User');
-
-// Function to hash the password
-const hashPassword = async (password) => {
-    try {
-        const saltRounds = 10;
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
-        return hashedPassword;
-    } catch (error) {
-        throw new Error('Error hashing password');
-    }
-};
-
-// Function to compare the provided password with the hashed password in the database
-const comparePasswords = async (password, hashedPassword) => {
-    try {
-        const match = await bcrypt.compare(password, hashedPassword);
-        return match;
-    } catch (error) {
-        throw new Error('Error comparing passwords');
-    }
-};
-
-// Function to authenticate a user based on their username and password
-const authenticateUser = async (username, password) => {
-    try {
-        const user = await User.findOne({ where: { username } });
-        // User not found
-        if (!user) {
-            return false;
-        }
-        const match = await comparePasswords(password, user.password);
-        // Authentication successful
-        if (match) {
-            return user;
-        // Password do not match
-        } else {
-            return false;
-        }
-    } catch (error) {
-        throw new Error('Error authenticating user');
-    }
-};
-
 module.exports = {
-    hashPassword,
-    authenticateUser,
-};
+    format_date: (date) => {
+        // Format date as MM/DD/YYYY
+        return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+    },
+    format_plural: (word, amount) => {
+        if (amount !== 1) {
+            return `${word}s`;
+        }
+        return word;
+    },
+    format_url: url => {
+        return url
+            .replace('http://', '')
+            .replace('https://', '')
+            .replace('www.', '')
+            .split('/')[0]
+            .split('?')[0];
+    },
+};  
