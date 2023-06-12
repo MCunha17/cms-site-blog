d4ece5051ac666629b5fa49065d354c5d30c6f19
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-06-12 12:10:38 -0500

    Added post.js route.

---
+++ routes/post.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/post');
+
+// Display a blog post
+router.get('/:postId', (req, res) => {
+    const postId = req.params.postId;
+
+    res.render('blogpost', { blogPost });
+});
+
+// Leave a comment on a blog post
+router.post('/:postId/comment', (req, res) => {
+    const postId = req.params.postId;
+    const { comment } = req.body;
+
+    res.redirect(`/posts/${postId}`);
+});
+
+module.exports = router;
