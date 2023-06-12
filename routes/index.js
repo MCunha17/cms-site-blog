5356fe29a6548088988da4403b0dd787a843d171
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-06-12 12:04:08 -0500

    Added index.js route.

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const { authenticateUser } = require('../utils/authentication');
+
+// Homepage route
+router.get('/', (req, res) => {
+    // Retrieve existing blog posts
+    const blogPosts = [];
+    res.render('homepage', { blogPosts });
+});
+
+// Navigation links route
+router.get('/navigation', (req, res) => {
+    if (req.session.user) {
+        // User is logged in
+        res.render('navigation-loggedin');
+    } else {
+        // User is not logged in
+        res.render('navigation-loggedout');
+    }
+});
+
+module.exports = router;
