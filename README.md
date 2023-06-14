a0fe9a823b248c08bf9edb33a5fb78ba700680da
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-06-14 18:10:42 -0500

    Updated post route.

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # cms-site-blog
-* npm express
-* 
+* npm init
+* npm install express
+* npm install express-handlebars 
+* npm install mysql2
+* npm install sequelize 
+* npm install dotenv
+* npm install bcrypt 
+* npm install express-session 
+* npm install connect-session-sequelize
--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,30 +1,26 @@
 const router = require('express').Router();
-const { Comment } = require('../../models');
-const withAuth = require('../../utils/withAuth');
+const { Post, Comment, User } = require('../../models');
 
-// Create a new comment
-router.post('/', withAuth, async (req, res) => {
+// POST /api/posts
+router.post('/', async (req, res) => {
   try {
-    const newComment = await Comment.create({
+    const newPost = await Post.create({
       ...req.body,
       user_id: req.session.user_id,
     });
-
-    res.json({ newComment, success: true });
+    res.json({ newPost, success: true });
   } catch (err) {
     res.sendStatus(500).send(err);
   }
 });
 
-// Update a comment
-router.put('/:id', withAuth, async (req, res) => {
+router.put('/:id', async (req, res) => {
   try {
-    await Comment.update(req.body, {
+    await Post.update(req.body, {
       where: {
         id: req.params.id,
       },
     });
-
     res.json({ success: true });
   } catch (err) {
     console.log(err);
@@ -32,11 +28,10 @@ router.put('/:id', withAuth, async (req, res) => {
   }
 });
 
-// Delete a comment
 router.delete('/:id', async (req, res) => {
   try {
-    // Delete one comment by its `id` value
-    await Comment.destroy({
+    // delete one post by its `id` value
+    await Post.destroy({
       where: {
         id: req.params.id,
       },
