1f85ab56c6c7a184e152612108c983f2057592b4
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-07-03 13:10:04 -0500

    Updated .env credentials

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,27 @@
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize(
-  'blog_posts',
-  'root',
-  'Abobora!19',
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
+let sequelize;
+
+// Check if a JAWSDB_URL environment variable is available (indicating a Heroku deployment)
+if (process.env.JAWSDB_URL) {
+  // Set up Sequelize connection using the JAWSDB_URL
+  sequelize = new Sequelize(process.env.JAWSDB_URL, {
+    dialect: 'mysql'
+  });
+} else {
+  // Set up Sequelize connection using the local environment variables
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
+      port: process.env.DB_PORT,
+      apiKey: process.env.API_KEY,
+    }
+  );
+}
 
 module.exports = sequelize;
